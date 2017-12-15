ce364b323123d31af9606972102da398ef3432e3
SebastianG <sebastian.garren@gmail.com>
2017-12-14 19:21:10 -0600

    Before Database Made

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> cheesecategory = context.Categories.ToList();
+
+            return View(cheesecategory);
+        }
+
+
+    }
+}
